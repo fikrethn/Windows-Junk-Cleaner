280be7c9c2593004e97fb05e54ddf9ebd9acd5cd
Fikrethan <83844701+fikrethn@users.noreply.github.com>
2026-09-05 20:43:19 +0300

    Enhance output messages in Windows Junk Cleaner

--- WindowsJunkCleaner.bat
+++ WindowsJunkCleaner.bat
@@ -2,35 +2,42 @@
 title Windows Junk Cleaner
 color 0A
 
-echo ==========================================
-echo          WINDOWS JUNK CLEANER
-echo ==========================================
 echo.
-echo Starting cleanup...
+echo  ==========================================
+echo.
+echo           WINDOWS JUNK CLEANER
+echo.
+echo           Author: Fikrethn
+echo           GitHub: github.com/fikrethn
+echo.
+echo  ==========================================
+echo.
+echo  Starting cleanup...
 echo.
 
-echo [1/4] Cleaning user TEMP...
+echo  [1/4] Cleaning user TEMP...
 del /f /s /q "%TEMP%\*" >nul 2>&1
 for /d %%D in ("%TEMP%\*") do rd /s /q "%%D" >nul 2>&1
 
-echo [2/4] Cleaning Windows TEMP...
+echo  [2/4] Cleaning Windows TEMP...
 del /f /s /q "%SystemRoot%\Temp\*" >nul 2>&1
 for /d %%D in ("%SystemRoot%\Temp\*") do rd /s /q "%%D" >nul 2>&1
 
-echo [3/4] Cleaning Windows Update cache...
+echo  [3/4] Cleaning Windows Update cache...
 del /f /s /q "%SystemRoot%\SoftwareDistribution\Download\*" >nul 2>&1
 for /d %%D in ("%SystemRoot%\SoftwareDistribution\Download\*") do rd /s /q "%%D" >nul 2>&1
 
-echo [4/4] Cleaning DirectX Shader Cache...
+echo  [4/4] Cleaning DirectX Shader Cache...
 del /f /s /q "%LocalAppData%\D3DSCache\*" >nul 2>&1
 for /d %%D in ("%LocalAppData%\D3DSCache\*") do rd /s /q "%%D" >nul 2>&1
 
 echo.
-echo ==========================================
-echo          CLEANUP COMPLETED
-echo ==========================================
+echo  ==========================================
 echo.
-echo Files currently in use were skipped.
-echo Author: https://github.com/fikrethn
+echo           CLEANUP COMPLETED
+echo.
+echo  Files currently in use were skipped.
+echo.
+echo  ==========================================
 echo.
 pause
