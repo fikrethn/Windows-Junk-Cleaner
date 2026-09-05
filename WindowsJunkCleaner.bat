1e1623b828793e7ab6f80d6b7f623676456dc43b
Fikrethan <83844701+fikrethn@users.noreply.github.com>
2026-09-05 20:41:31 +0300

    Update WindowsJunkCleaner.bat

--- WindowsJunkCleaner.bat
+++ WindowsJunkCleaner.bat
@@ -1,35 +1,36 @@
 @echo off
-title Windows Gereksiz Dosya Temizleyici
+title Windows Junk Cleaner
 color 0A
 
 echo ==========================================
-echo       WINDOWS GEREKSIZ DOSYA TEMIZLEYICI
+echo          WINDOWS JUNK CLEANER
 echo ==========================================
 echo.
-echo Temizlik basliyor...
+echo Starting cleanup...
 echo.
 
-echo [1/4] Kullanici TEMP temizleniyor...
+echo [1/4] Cleaning user TEMP...
 del /f /s /q "%TEMP%\*" >nul 2>&1
 for /d %%D in ("%TEMP%\*") do rd /s /q "%%D" >nul 2>&1
 
-echo [2/4] Windows TEMP temizleniyor...
+echo [2/4] Cleaning Windows TEMP...
 del /f /s /q "%SystemRoot%\Temp\*" >nul 2>&1
 for /d %%D in ("%SystemRoot%\Temp\*") do rd /s /q "%%D" >nul 2>&1
 
-echo [3/4] Windows Update onbellegi temizleniyor...
+echo [3/4] Cleaning Windows Update cache...
 del /f /s /q "%SystemRoot%\SoftwareDistribution\Download\*" >nul 2>&1
 for /d %%D in ("%SystemRoot%\SoftwareDistribution\Download\*") do rd /s /q "%%D" >nul 2>&1
 
-echo [4/4] DirectX Shader Cache temizleniyor...
+echo [4/4] Cleaning DirectX Shader Cache...
 del /f /s /q "%LocalAppData%\D3DSCache\*" >nul 2>&1
 for /d %%D in ("%LocalAppData%\D3DSCache\*") do rd /s /q "%%D" >nul 2>&1
 
 echo.
 echo ==========================================
-echo          TEMIZLIK TAMAMLANDI
+echo          CLEANUP COMPLETED
 echo ==========================================
 echo.
-echo Kullanilmakta olan dosyalar atlandi.
+echo Files currently in use were skipped.
+echo Author: https://github.com/fikrethn
 echo.
 pause
